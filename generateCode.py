0b2edb06edaee5a107d890d77981a69e4199078b
Nicolas Hug <niourf@gmail.com>
2015-08-25 11:43:30 +0200

    app code. README coming soon

---
+++ generateCode.py
@@ -0,0 +1,169 @@
+#!/usr/bin/python
+
+import sys
+
+import ply.yacc as yacc
+import ply.lex as lex
+
+import tools as t
+
+
+### LEXER SPECS ###
+tokens = (
+    'RUN', 'FLOAT_VAL', 'INT_VAL', 'REPEAT', 'KM', 'KMH', 'PERCENT', 'INDEF',
+    'AT', 'TIME_VALS', 'LBRACKET', 'RBRACKET', 'SEMI', 'TIME_VAL', 'PREFIX',
+    'MORELESS'
+)
+
+t_RUN               = r'run'
+t_REPEAT            = r'repeat'
+t_KM                = r'km' 
+t_KMH               = r'kmh' 
+t_PERCENT           = r'%'
+t_INDEF             = r'indefinitely'
+t_AT                = r'at'
+t_TIME_VALS         = r'times'
+t_LBRACKET          = r'\{'
+t_RBRACKET          = r'\}'
+t_SEMI              = r';'
+t_PREFIX            = r'\(\w+\)'
+t_MORELESS          = r'\+-'
+t_ignore            = " \t"
+t_ignore_COMMENT    = r'\#.*' 
+
+def t_FLOAT_VAL(t):
+    r'\d+\.\d+'
+
+    t.value = float(t.value)
+    return t
+
+def t_TIME_VAL(t):
+    r'[0-9]+m[0-9]+s'
+
+    # remove 'm' and 's' from string and return the value in seconds
+    mins, secs = t.value.split('m')
+    secs = secs[:-1]
+    t.value = int(mins) * 60 + int(secs)
+    return t
+
+def t_INT_VAL(t):
+    r'\d+'
+
+    t.value = int(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print ("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+
+
+### PARSER SPECS ###
+def p_trainingSession(p):
+    '''trainingSession : step SEMI
+                      | step SEMI trainingSession'''
+    
+    # return the session : a list of steps (runStep or repeat)
+    if len(p) == 3: 
+        p[0] = [p[1]] 
+    else:
+        session = p[3]
+        session.insert(0, p[1])
+        p[0] = session 
+
+def p_step(p):
+    '''step : RUN duration prefix
+            | RUN duration AT target prefix
+            | REPEAT INT_VAL TIME_VALS LBRACKET trainingSession RBRACKET'''
+
+    if len(p) == 4:
+        kwargs = dict(p[2], **p[3]) # combine the two dicts of argument
+        remaining = t.Remaining(**kwargs)
+        p[0] = t.RunStep(remaining)
+    elif len(p) == 6:
+        kwargs = dict(p[2], **p[5]) # combine the two dicts of arguments
+        remaining = t.Remaining(**kwargs)
+        p[0] = t.RunStep(remaining, p[4])
+    else:
+        p[0] = t.Repeat(p[2], p[5])
+
+def p_prefix(p):
+    '''prefix : PREFIX
+              | '''
+    if len(p) == 2: 
+        # remove parenteses and set prefix argument
+        p[0] = {'prefix' : '"' + p[1][1:-1] + '"'}
+    else:
+        p[0] = {}
+
+def p_target_percent(p):
+    '''target : INT_VAL PERCENT margin
+              | FLOAT_VAL KMH margin
+              | INT_VAL KMH margin '''
+
+    kwargs = p[3] # the margin
+    if p[2] == '%':
+        p[0] = t.Target(hr=p[1], **kwargs)
+    else:
+        p[0] = t.Target(spd=p[1], **kwargs)
+
+def p_margin(p):
+    '''margin : MORELESS INT_VAL
+              | MORELESS FLOAT_VAL
+              | '''
+    if len(p) == 3:
+        p[0] = {'margin': p[2]}
+    else:
+        p[0] = {}
+
+def p_duration(p):
+    '''duration : FLOAT_VAL KM
+                | INT_VAL KM
+                | INDEF
+                | TIME_VAL'''
+
+    if len(p) == 3:
+        p[0] = {'dist' : p[1]}
+    elif p[1] == 'indefinitely':
+        p[0] = {'lap' : True}
+    else:
+        p[0] = {'dur' : p[1]}
+
+
+def p_error(p):
+    if p:
+        print ("Syntax error at '%s'" % p.value)
+    else:
+        print ("Syntax error at EOF")
+
+
+
+### MAIN PROGRAM ###
+if sys.version_info[0] >= 3:
+    raw_input = input
+
+if len(sys.argv) != 2:
+    print("will use 'input' as input file")
+    inFile = inFile = open('example.txt', 'r')
+else:
+    inFile = inFile = open(sys.argv[1], 'r')
+
+lex.lex() # build lexer
+yacc.yacc() # build parser
+
+# read and parse input file
+# result is in the 'session' list containing training steps
+
+session = yacc.parse(inFile.read())
+inFile.close()
+
+print("Remaning app, showing how much time/distance is left:")
+print(t.applicationCode(session))
+print("-" * 30)
+print("Target app, showing if you're higher or lower than your target:")
+print(t.applicationCode(session, appType='target'))
---
+++ tools.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python3
+
+class Target:
+    """Target class is a data structure used in the RunStep class
+         
+        A target is either a percentage of the user's maximal heart rate, or a
+        speed in km/h (not both at the same time). Runner will be told to run
+        between at the corresponding target with a given margin. 
+    """
+    #@TODO : use the prefix
+
+    def __init__(self, hr=0, spd=0, margin=1., prefix=None):
+        self.hr = hr
+        self.spd = spd
+
+        self.hrMin = hr - margin if hr else 0
+        self.hrMax = hr + margin if hr else 0
+        self.spdMin = spd - margin if spd else 0
+        self.spdMax = spd + margin if spd else 0
+
+        self.prefix = prefix
+
+class Remaining:
+    """Remaining class is a data structure used for the RunStep class
+
+       It determines what is left to complete the current step in the training
+       session, and move on to the next one. 'What is left' can be either a
+       duration (in seconds), a distance (in km), or undefined. In the latter
+       case, the next step only starts when the user pushes the 'lap'
+       button. A prefix can be specified to give information on the current
+       step, such as "warmup", or "cooldown".
+    """
+
+    def __init__(self, dur=0, dist=0, lap=False, prefix='"run"'):
+        self.dur = dur
+        self.dist = dist
+        self.lap = lap
+
+        self.prefix = prefix
+        self.postfix = '"s"' if self.dur != 0 or self.lap else '"km"'
+
+class RunStep:
+    """RunStep class is used to represent a step where the user has to run
+       
+       There are two attributes: remaining for the first app, and target for
+       the target app that can be None.
+    """
+
+
+    def __init__(self, remaining, target=None): 
+        self.remaining = remaining
+        self.target = target
+
+    def transitionCode(self, withBip=False):
+        """return the code that is common to both remaining and target app. It
+        is the code that will generate the transitions between the different
+        steps"""
+
+        out = ""
+        if self.remaining.dur != 0:
+            out += ("  if (SUUNTO_DURATION - last_step_duration >= " +
+            str(self.remaining.dur) + ") {\n")
+        elif self.remaining.dist != 0:
+            out += ("  if (SUUNTO_DISTANCE - last_step_distance >= " +
+            str(self.remaining.dist) + ") {\n")
+        elif self.remaining.lap:
+            out += "  if (SUUNTO_LAP_NUMBER > current_lap_number) {\n"
+            out += "    current_lap_number = current_lap_number + 1;\n"
+
+
+        out += "    last_step_duration = SUUNTO_DURATION;\n"
+        out += "    last_step_distance = SUUNTO_DISTANCE;\n"
+        if withBip:
+            out += "    Suunto.alarmBeep();\n"
+            out += "    Suunto.light();\n"
+        out += "    step = step + 1;\n"
+        out += "  }"
+
+        return out
+
+    def remainingApp(self):
+        """return the code of the step for the remaining app"""
+
+        out = ""
+
+        out += "  prefix = " + self.remaining.prefix + ";\n"
+        out += "  postfix = " + self.remaining.postfix + ";\n"
+
+        if self.remaining.dur != 0:
+            out += ("  RESULT = last_step_duration + " +
+            str(self.remaining.dur) + " - SUUNTO_DURATION;\n")
+        elif self.remaining.dist != 0:
+            out += ("  RESULT = last_step_distance + " +
+            str(self.remaining.dist) + " - SUUNTO_DISTANCE;\n")
+        elif self.remaining.lap:
+            out += "  RESULT = SUUNTO_LAP_DURATION;\n"
+
+        out += self.transitionCode(withBip=True)
+
+        return out
+
+    def targetApp(self):
+        """return the code of the step for the target app"""
+        out = ""
+
+        if self.target:
+            if self.target.hrMax != 0:
+                if not self.target.prefix : self.target.prefix = '"HR"'
+                out += '  prefix = ' + self.target.prefix + ';\n'
+                out += "  RESULT = " + str(self.target.hr) + ";\n";
+                out += ("  if (" + str(self.target.hrMax) + " < (SUUNTO_HR *100 / " + 
+                "SUUNTO_USER_MAX_HR)) {\n")
+                out += '    postfix = "++";\n  }'
+                out += ("  else if (" + str(self.target.hrMin) + " > " + 
+                "(SUUNTO_HR * 100 / SUUNTO_USER_MAX_HR)) {\n")
+                out += '    postfix = "--";\n  }'
+                out += "  else {\n"
+                out += '    postfix = "ok";\n  }\n'
+                
+
+            elif self.target.spdMax != 0:
+                if not self.target.prefix : self.target.prefix = '"spd"'
+                out += '  prefix = ' + self.target.prefix + ';\n'
+                out += "  RESULT = " + str(self.target.spd) + ";\n";
+                out += ("  if (" + str(self.target.spdMax) + 
+                " < SUUNTO_SPEED) {\n")
+                out += '    postfix = "++";\n  }'
+                out += ("  else if (" + str(self.target.spdMin) + 
+                " > SUUNTO_SPEED) {\n")
+                out += '    postfix = "--";\n  }'
+                out += "  else {\n"
+                out += '    postfix = "ok";\n  }\n'
+
+        out += self.transitionCode()
+
+        return out
+
+class Repeat:
+    """Repeat class used to represent repetition in a training program. Useful
+    for interval training."""
+
+    def __init__(self, nRepeat, runSteps):
+        self.nRepeat = nRepeat # number of repetitions
+        self.runSteps = runSteps # the list of Runsteps instances to repeat
+        self.nSteps = len(self.runSteps)
+
+def applicationCode(session, appType='remaining'):
+    """ take a training session as input and return the code for the
+    corresponding application. Application can be either 'remaining' or
+    'target' """
+
+    if appType == 'remaining':
+        stepCode = lambda step: step.remainingApp()
+    else: 
+        stepCode = lambda step: step.targetApp()
+
+    out = ""
+    stepi = 0
+
+    for step in session:
+        if isinstance(step, Repeat):
+            for ssi, subStep in enumerate(step.runSteps):
+                out += "if (" 
+                for n in range(0, step.nRepeat * step.nSteps, step.nSteps):
+                    out += "step == " + str(stepi + ssi + n) + " || "
+                out += "0){\n" 
+                out += stepCode(subStep)
+                out += "\n}\n\n"
+            stepi += step.nSteps * step.nRepeat
+                
+        else :
+            out += "if (step == " + str(stepi) + ") {\n"
+            out += stepCode(step)
+            out += "\n}\n\n"
+            stepi += 1
+
+    return out
